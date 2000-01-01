87e5e79ce80ba6cbea83a63b23dfb64da017fb9a
i3oot <tobias.waller@googlemail.com>
2000-01-01 00:07:54 +0000

    renamed libpixel to libneopixel in main build process

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 make 
 swig -c++ -python pixel.i
 g++ -fPIC -shared -lsupc++ -std=c++11 -o pixel_wrap.o -c pixel_wrap.cxx -I /usr/include/python2.7/
-gcc -shared -Wl,-soname,libpixel.so.1 -o libpixel.so  *.o -lpython2.7 -lstdc++ -Lam335x/app_loader/lib/ -lprussdrv -lprussdrvd 
+gcc -shared -Wl,-soname,libneopixel.so.1 -o libneopixel.so  *.o -lpython2.7 -lstdc++ -Lam335x/app_loader/lib/ -lprussdrv -lprussdrvd 
 rm -f _pixel.so
-ln libpixel.so _pixel.so
+ln libneopixel.so _pixel.so
 
 pushd ipk
 ./buildipk.sh
