a59267f5b49185b32c2a9f9596581c6cfd2f0b39
i3oot <tobias.waller@googlemail.com>
2014-05-31 12:03:54 +0000

    removed c&p fail

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 make 
 swig -c++ -python pixel.i
 g++ -fPIC -shared -lsupc++ -std=c++11 -o pixel_wrap.o -c pixel_wrap.cxx -I /usr/include/python2.7/
-gcc -shared -Wl,-soname,libpixel.so.1 -o libpixel.so  *.o -lpython2.7 -lstdc++ -L. -lprussdrv -lprussdrvd 
 gcc -shared -Wl,-soname,libpixel.so.1 -o libpixel.so  *.o -lpython2.7 -lstdc++ -Lam335x/app_loader/lib/ -lprussdrv -lprussdrvd 
 ln libpixel.so _pixel.so
