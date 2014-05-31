ddfc7a419555372231f6717b42981c6fe3e6bce5
i3oot <tobias.waller@googlemail.com>
2014-05-31 14:29:02 +0000

    automatic versioning

--- ipk/buildipk.sh
+++ ipk/buildipk.sh
@@ -3,10 +3,13 @@
 rm -rf dist
 mkdir dist
 
+export version=`date +%Y%m%d%H%M%S`
+
 rm -rf libpixel-dev/usr
 mkdir -p libpixel-dev/usr/include/libpixel
 cp ../*.h libpixel-dev/usr/include/libpixel/
 cp ../*.hpp libpixel-dev/usr/include/libpixel/
+sed "s/%DATE%/$version/g" libpixel-dev/CONTROL/control.template > libpixel-dev/CONTROL/control
 
 pushd dist
 ../ipkg-build ../libpixel-dev .
@@ -21,6 +24,8 @@ mkdir -p libpixel/usr/lib
 cp ../*bin libpixel/usr/lib/
 cp ../*so libpixel/usr//lib/
 cp  ../am335x/app_loader/lib/*.so libpixel/usr/lib/
+sed "s/%DATE%/$version/g" libpixel/CONTROL/control.template > libpixel/CONTROL/control
+
 
 pushd dist
 ../ipkg-build ../libpixel .
