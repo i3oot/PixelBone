54ce68dced8a2318390c2e19c3b7e96146b3054f
i3oot <tobias.waller@googlemail.com>
2014-06-01 14:13:09 +0200

    Update helloWorld.cpp

--- HelloWorld/helloWorld.cpp
+++ HelloWorld/helloWorld.cpp
@@ -1,7 +1,7 @@
 #include <libneopixel/pixel.hpp>
 int main(void) {
   PixelBone_Pixel *const neoPixelStick = new PixelBone_Pixel(8);
-  for (unsigned led = 0; led < neoPixelStick->numPixels(); p++) {
+  for (unsigned led = 0; led < neoPixelStick->numPixels(); led++) {
       neoPixelStick->setPixelColor(led, 128, 128, 128);
   }
   neoPixelStick->show();
