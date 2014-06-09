dfb3f50a4dcad52f8c14b78d3722ee88076ef20a
i3oot <tobias.waller@googlemail.com>
2014-06-09 11:05:18 +0200

    Renamed libpixel to libneopixel in build process

--- ipk/buildipk.sh
+++ ipk/buildipk.sh
@@ -5,51 +5,51 @@ mkdir dist
 
 export version=`date +%Y%m%d%H%M%S`
 
-rm -rf libpixel-dev/usr
-mkdir -p libpixel-dev/usr/include/libpixel
-cp ../*.h libpixel-dev/usr/include/libpixel/
-cp ../*.hpp libpixel-dev/usr/include/libpixel/
-sed "s/%DATE%/$version/g" libpixel-dev/CONTROL/control.template > libpixel-dev/CONTROL/control
+rm -rf libneopixel-dev/usr
+mkdir -p libneopixel-dev/usr/include/libneopixel
+cp ../*.h libneopixel-dev/usr/include/libneopixel/
+cp ../*.hpp libneopixel-dev/usr/include/libneopixel/
+sed "s/%DATE%/$version/g" libneopixel-dev/CONTROL/control.template > libneopixel-dev/CONTROL/control
 
 pushd dist
-../ipkg-build ../libpixel-dev .
+../ipkg-build ../libneopixel-dev .
 popd 
 
 
 
 
 
-rm -rf libpixel/usr
-mkdir -p libpixel/usr/lib
-cp ../*bin libpixel/usr/lib/
-cp ../*so libpixel/usr//lib/
-cp  ../am335x/app_loader/lib/*.so libpixel/usr/lib/
-ln libpixel/usr/lib/libpixel.so libpixel/usr/lib/libpixel.so.1 
-sed "s/%DATE%/$version/g" libpixel/CONTROL/control.template > libpixel/CONTROL/control
+rm -rf libneopixel/usr
+mkdir -p libneopixel/usr/lib
+cp ../*bin libneopixel/usr/lib/
+cp ../*so libneopixel/usr//lib/
+cp  ../am335x/app_loader/lib/*.so libneopixel/usr/lib/
+ln libneopixel/usr/lib/libneopixel.so libneopixel/usr/lib/libneopixel.so.1 
+sed "s/%DATE%/$version/g" libneopixel/CONTROL/control.template > libneopixel/CONTROL/control
 
 
 pushd dist
-../ipkg-build ../libpixel .
+../ipkg-build ../libneopixel .
 popd
 
 
-rm -rf libpixel-python/usr
-mkdir -p libpixel-python/usr/lib/python2.7
-cp ../_pixel.so libpixel-python/usr/lib/python2.7/
-cp ../pixel.py libpixel-python/usr/lib/python2.7/
-sed "s/%DATE%/$version/g" libpixel-python/CONTROL/control.template > libpixel-python/CONTROL/control
+rm -rf libneopixel-python/usr
+mkdir -p libneopixel-python/usr/lib/python2.7
+cp ../_pixel.so libneopixel-python/usr/lib/python2.7/
+cp ../pixel.py libneopixel-python/usr/lib/python2.7/
+sed "s/%DATE%/$version/g" libneopixel-python/CONTROL/control.template > libneopixel-python/CONTROL/control
 
 
 pushd dist
-../ipkg-build ../libpixel-python .
+../ipkg-build ../libneopixel-python .
 popd
 
-rm -rf libpixel-devicetree/boot
-mkdir -p libpixel-devicetree/boot
-cp ../dirtree/am335x-boneblack.dtb libpixel-devicetree/boot/
-sed "s/%DATE%/$version/g" libpixel-devicetree/CONTROL/control.template > libpixel-devicetree/CONTROL/control
+rm -rf libneopixel-devicetree/boot
+mkdir -p libneopixel-devicetree/boot
+cp ../dirtree/am335x-boneblack.dtb libneopixel-devicetree/boot/
+sed "s/%DATE%/$version/g" libneopixel-devicetree/CONTROL/control.template > libneopixel-devicetree/CONTROL/control
 
 
 pushd dist
-../ipkg-build ../libpixel-devicetree .
+../ipkg-build ../libneopixel-devicetree .
 popd
